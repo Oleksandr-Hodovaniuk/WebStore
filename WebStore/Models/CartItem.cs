4387dd804880d9de878aa07cf997f1a845a711b9
OleksanderHodovaniuk <s.godovanuk@gmail.com>
2023-09-05 22:50:16 +0300

    Create models for database tables

---
+++ WebStore/Models/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Mobileshop.Models
+{
+    public class CartItem
+    {
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public User User { get; set; }
+        public Product Product { get; set; }
+    }
+}
