62d4c0dc4a0b018e44aaeaeb0ea523916aaa82fd
OleksanderHodovaniuk <s.godovanuk@gmail.com>
2023-09-08 22:36:01 +0300

    Create LogInController and register ILogInService

---
+++ WebStore/Controllers/LogInController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces;
+using WebStore.Models.DTOs;
+using WebStore.Models.VeiwModels;
+
+namespace WebStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogInController : ControllerBase
+    {
+        //Add ILogInService.
+        private readonly ILogInService logInService;
+        public LogInController(ILogInService logInService)
+        {
+            this.logInService = logInService;
+        }
+
+        //Log in method.
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> LogIn(LogInViewModel model)
+        {
+            try
+            {
+                var user = await logInService.LogIn(model);
+
+                if (user != null)
+                    return user;
+
+                return BadRequest("Incorrect data.");
+            }
+            catch (Exception ex) 
+            {
+                BadRequest(ex.Message);
+            }
+
+            return BadRequest();
+        }
+    }
+}
