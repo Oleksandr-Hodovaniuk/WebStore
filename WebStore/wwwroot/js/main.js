8f882756c9655c819ab9a35ef7a8416a88e4fc8c
OleksanderHodovaniuk <s.godovanuk@gmail.com>
2023-09-16 19:10:40 +0300

    Create getAllProducts method

--- WebStore/wwwroot/js/main.js
+++ WebStore/wwwroot/js/main.js
@@ -1,1 +1,18 @@
-﻿
+﻿var mainDiv = document.getElementsByClassName("mainDiv")[0];
+
+//Get all products from database.
+async function getAllProducts()
+{
+    const responce = await fetch("/api/Product",
+    {
+        method: "GET",
+        headers: {"Accept": "application/json", "Content-Type": "application/json"}
+    });
+
+    if(responce.ok === true)
+    {
+        const productList = await responce.json();
+    }
+}
+
+getAllProducts();
