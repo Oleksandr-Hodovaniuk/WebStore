c5abc7445824735f591eee0de40bf1abc96affed
OleksanderHodovaniuk <s.godovanuk@gmail.com>
2023-09-17 18:31:25 +0300

    Add styles for products

--- WebStore/wwwroot/js/main.js
+++ WebStore/wwwroot/js/main.js
@@ -21,29 +21,37 @@ async function getAllProducts()
 function displayProduct(data)
 {
     const product = document.createElement("div");
+    product.className = "product";
 
     const verticalContainer = document.createElement("div");
     verticalContainer.className = "verticalContainer";
 
     const image = document.createElement("img");
     image.src = "data:image/png;base64," + data.image;
+    image.className = "image";
     verticalContainer.append(image);
 
     const name = document.createElement("div");
     name.innerText = data.name;
+    name.className = "name";
     verticalContainer.append(name);
 
     const description = document.createElement("div");
     description.innerText = data.description;
+    description.className = "description";
     verticalContainer.append(description);
 
     const horizontalContainer = document.createElement("div");
+    horizontalContainer.className = "horizontalContainer";
+
     const price = document.createElement("div");
     price.innerText = data.price + " ₴";
+    price.className = "price";
     horizontalContainer.append(price);
 
     const addBtn = document.createElement("button");
     addBtn.innerText = "Add to cart";
+    addBtn.className = "add";
     horizontalContainer.append(addBtn);
     
     product.append(verticalContainer, horizontalContainer);
