eea7550e1cd629d2bdaeecd853384fe421cfc235
OleksanderHodovaniuk <s.godovanuk@gmail.com>
2023-10-25 16:12:41 +0300

    Email collect selected products

--- WebStore/Services/EmailService.cs
+++ WebStore/Services/EmailService.cs
@@ -8,6 +8,8 @@ using MailKit.Net.Smtp;
 using MailKit.Security;
 using SmtpClient = MailKit.Net.Smtp.SmtpClient;
 using WebStore.Models;
+using WebStore.Models.DTOs;
+using Mobileshop.Models;
 
 namespace WebStore.Services
 {
@@ -38,20 +40,35 @@ namespace WebStore.Services
                     var message = new MimeMessage();
                     message.From.Add(new MailboxAddress("Admin", "mobileshop@gmail.com"));
                     message.To.Add(new MailboxAddress("Client", "s.godovanuk@gmail.com"));
-                    message.Subject = $"Congratulations, {user.Name}, on the successful purchase of our product(s) " +
-                        $"in our web store! Your products count: {products.ProductsId.Length}; Sum of your purshase: {price} ₴";
+                    message.Subject = $"Congratulations, {user.Name}, on the successfull purchase of our product(s) " +
+                        $"in our web store! Your products count: {products.ProductsId.Length}; Sum of your purchase: {price} ₴";
 
-                    //string htmlContent = File.ReadAllText("email.html");
+                    var purchesList = new List<ProductDTO>();
+
+                    var productList = await context.Products.ToListAsync();
+
+                    foreach(var id in products.ProductsId) 
+                    {
+                        foreach (var product in productList)
+                        {
+                            if (id == product.Id)
+                            {
+                                purchesList.Add(ProductToDTO(product));
+                            }
+                        }
+                    }
+
+                    string htmlContent = File.ReadAllText("wwwroot/email.html");
 
                     var builder = new BodyBuilder();
-                    builder.HtmlBody = "<h1>Test</h1>";
+                    builder.HtmlBody = htmlContent;
 
                     message.Body = builder.ToMessageBody();
 
                     using (var client = new SmtpClient())
                     {
                         await client.ConnectAsync("smtp.gmail.com", 587, false);
-                        await client.AuthenticateAsync("s.godovanuk@gmail.com", "gzct vplc ntqe ffsb");
+                        await client.AuthenticateAsync("s.godovanuk@gmail.com", "mdgg moke lnau mxua");
                         await client.SendAsync(message);
                         await client.DisconnectAsync(true);
                     }
@@ -68,8 +85,21 @@ namespace WebStore.Services
             else 
             {
                 Console.WriteLine("Error!");
-            }
-            
+            }  
+        }
+
+        private ProductDTO ProductToDTO(Product product)
+        {
+            var dto = new ProductDTO()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                Image = Convert.ToBase64String(product.Image)
+            };
+
+            return dto;
         }
     }
 }
